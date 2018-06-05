9b6c09220610942f9bda2ad41a8764f107f07767
Jimmy McNamara and Jim Palowski <jimmy@>
2018-06-05 14:33:44 -0700

    Add edit options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,17 +9,34 @@ import { Task } from './models/task.model';
 export class AppComponent {
   currentFocus: string = 'Angular Homework';
   currentTime = new Date();
+
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
+    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('Begin brainstorming possible JavaScript group projects', 2),
+    new Task('Add README file to last few Angular repos on GitHub', 2)
   ];
+  selectedTask = null;
+  finishedEditing() {
+    this.selectedTask = null;
   }
- export class Task{
-   public done: boolean = false;
-   constructor(public description: string){ }
-
- }
+  priorityColor(currentTask){
+    if(currentTask.priority === 3){
+      return "bg-danger";
+    } else if (currentTask.priority === 2){
+      return "bg-warning";
+    }else {
+      return "bg-info";
+    }
+  }
+  editTask(clickedTask){
+    this.selectedTask = clickedTask;
+  }
+  }
+ // export class Task{
+ //   public done: boolean = false;
+ //   constructor(public description: string){ }
+ //
+ // }
